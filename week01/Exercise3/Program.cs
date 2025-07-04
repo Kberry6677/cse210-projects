f66f82f070eb0e9fba1918606a80cb4d2e034f5e
Krista Berry <kilroy6677@gmail.com>
2025-07-03 19:15:02 -0500

    Updating Week01 Exercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool keepPlaying;
+        do
+        {
+            keepPlaying = false;
+            Boolean correct = false;
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+            int count = 0;
+            while (!correct)
+            {
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+                count += 1;
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine($"You guessed it in {count} tries!");
+                    correct = true;
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }                
+            }
+
+            Console.WriteLine("Would you like to play again? (Y/N) ");
+            char playAgain = char.Parse(Console.ReadLine());
+            if (playAgain == 'Y' || playAgain == 'y')
+            {
+                keepPlaying = true;
+            }
+        } while (keepPlaying == true);
     }
 }
